594bd48a3eade2376c3332bf9679a6dd4c3e1888
Heric Felix Martins <hfelixmartins@gmail.com>
2022-05-12 20:50:45 -0300

    feat: structure created

---
+++ apps/api/ormconfig.ts
@@ -0,0 +1,13 @@
+export default {
+  type: 'mongodb',
+  synchronize: true,
+  logging: false,
+  entities: ['src/entities/**/*.ts'],
+  migrations: ['src/migrations/**/*.ts'],
+  subscribers: ['src/subscriber/**/*.ts'],
+  cli: {
+    entitiesDir: 'src/entities',
+    migrationsDir: 'src/migrations',
+    subscribersDir: 'src/subscriber',
+  },
+};
