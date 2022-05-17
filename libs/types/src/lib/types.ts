452ac8fb275ccdda7a889b07e491ffcdc1765b41
Heric Felix Martins <hfelixmartins@gmail.com>
2022-05-17 17:37:06 -0300

    feat: Article Repository created

--- libs/types/src/lib/types.ts
+++ libs/types/src/lib/types.ts
@@ -7,3 +7,16 @@ export interface EventsInterface {
   id: string;
   provider: string;
 }
+
+export interface ArticleInterface {
+  id?: string;
+  featured: boolean;
+  title: string;
+  url: string;
+  imageUrl: string;
+  newsSite: string;
+  summary: string;
+  publishedAt: string;
+  launches: LaunchesInterface[];
+  events: EventsInterface[];
+}
